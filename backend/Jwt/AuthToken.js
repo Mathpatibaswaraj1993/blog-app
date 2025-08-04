0191757338b2cffe3bb13f81375fd6c666ed649f
https://github.com/Mathpatibaswaraj1993/HTMLProject.git <mathpatibaswaraj@gmail.com>
2025-08-05 01:22:46 +0530

    c4

--- backend/Jwt/AuthToken.js
+++ backend/Jwt/AuthToken.js
@@ -7,7 +7,7 @@ const createTokenAndSaveCookies = async (userId, res) => {
   });
 
   res.cookie("jwt", token, {
-    httpOnly: false,
+    httpOnly: true,
     secure: true,
     sameSite: "none",
     maxAge:7 * 24 * 60 * 60 * 1000,
